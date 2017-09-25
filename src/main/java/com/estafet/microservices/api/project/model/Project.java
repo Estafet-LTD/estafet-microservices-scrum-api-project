913930d5c155851c6dd3f494e1dca8760c8e7f58
dennis williams <dennis.williams@estafet.com>
2017-09-25 11:07:12 +0100

    Tempo

--- src/main/java/com/estafet/microservices/api/project/model/Project.java
+++ src/main/java/com/estafet/microservices/api/project/model/Project.java
@@ -3,10 +3,25 @@ package com.estafet.microservices.api.project.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PROJECT")
 public class Project {
 
+	@Id
+	@SequenceGenerator(name = "PROJECT_ID_SEQ", sequenceName = "PROJECT_ID_SEQ", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PROJECT_ID_SEQ")
+	@Column(name = "PROJECT_ID")
 	private int id;
 
+	@Column(name = "TITLE", nullable = false)
 	private String title;
 
 	private List<Sprint> sprints = new ArrayList<Sprint>();
