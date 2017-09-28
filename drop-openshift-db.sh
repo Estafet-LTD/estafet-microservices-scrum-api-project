aab8b5726f6a3e1b58d0c04c96bc6790b5a66e35
dennis williams <dennis.williams@estafet.com>
2017-09-28 10:44:06 +0100

    Fixed the create and drop shell scripts

--- drop-openshift-db.sh
+++ drop-openshift-db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Executes the drop db schema
-psql -h $POSTGRESQL_SERVICE_HOST -p $POSTGRESQL_SERVICE_PORT -U postgres project-repository < drop-project-api-db.ddl
+psql -h $POSTGRESQL_SERVICE_HOST -p $POSTGRESQL_SERVICE_PORT -U postgres project-api < drop-project-api-db.ddl
