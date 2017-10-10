5e5cf32e64b466b100cb8e23a754d0e0642544f2
dennis williams <dennis.williams@estafet.com>
2017-10-10 13:30:02 +0100

    Objects sent must now be serializable.

--- src/main/java/com/estafet/microservices/api/project/model/Project.java
+++ src/main/java/com/estafet/microservices/api/project/model/Project.java
@@ -1,5 +1,7 @@
 package com.estafet.microservices.api.project.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +12,12 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "PROJECT")
-public class Project {
+public class Project implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6979565319684514092L;
 
 	@Id
 	@SequenceGenerator(name = "PROJECT_ID_SEQ", sequenceName = "PROJECT_ID_SEQ", allocationSize = 1)
