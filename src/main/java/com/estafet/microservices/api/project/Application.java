e91c90d6ce0880acc922647c28418bee799cccf8
dennis williams <dennis.williams@estafet.com>
2017-10-11 11:39:13 +0100

    Removed instrumentation to debug some of the issues service by service.

--- src/main/java/com/estafet/microservices/api/project/Application.java
+++ src/main/java/com/estafet/microservices/api/project/Application.java
@@ -34,12 +34,12 @@ public class Application extends SpringBootServletInitializer {
 		return application.sources(Application.class);
 	}
 
-	@Bean
+/*	@Bean
 	public io.opentracing.Tracer jaegerTracer() {
 		return new com.uber.jaeger.Configuration("project-api",
 				com.uber.jaeger.Configuration.SamplerConfiguration.fromEnv(),
 				com.uber.jaeger.Configuration.ReporterConfiguration.fromEnv()).getTracer();
-	}
+	}*/
 
 	@Bean
 	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
