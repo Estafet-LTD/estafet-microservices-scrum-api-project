0fa349b1f50b3ae61d5e2864d9d3e0e0a6c16c55
Dennis Williams <dennis.williams@estafet.com>
2018-05-01 20:31:54 +0100

    fixed build and verify deployment

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -21,25 +21,25 @@ node("maven") {
 	}
 
 	stage("build & deploy container") {
-		openshiftBuild namespace: ${project}, buildConfig: ${microservice}, showBuildLogs: "true",  waitTime: "3000000"
+		openshiftBuild namespace: $project, buildConfig: $microservice, showBuildLogs: "true",  waitTime: "3000000"
 	}
   	  
 	stage("verify container deployment") {
-		openshiftVerifyDeployment namespace: ${project}, depCfg: ${microservice}, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"	
+		openshiftVerifyDeployment namespace: $project, depCfg: $microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"	
 	}
 
 	stage("execute the container tests") {
 		withEnv(
-			[	"PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/project-api", 
+			[	"PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
 				"PROJECT_API_DB_USER=postgres", 
 				"PROJECT_API_DB_PASSWORD=welcome1",
-				"PROJECT_API_SERVICE_URI=http://project-api.${project}.svc:8080",
+				"PROJECT_API_SERVICE_URI=http://${microservice}.${project}.svc:8080",
 				"JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616",
 				"JBOSS_A_MQ_BROKER_USER=amq",
 				"JBOSS_A_MQ_BROKER_PASSWORD=amq"
 			]) {
-			sh "mvn verify -P integration-test"
-		}
+				sh "mvn verify -P integration-test"
+			}
 	}
 	
 	post {
