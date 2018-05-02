2beedb43bee092c8fc9534171d47dba41a60f195
Dennis Williams <dennis.williams@estafet.com>
2018-05-02 22:02:10 +0100

    service passed as param

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -2,7 +2,7 @@ def project = "test"
 def microservice = "project-api"
 
 @NonCPS
-def getImage(String json) {
+def getImage(String json, String microservice) {
 	def item = new groovy.json.JsonSlurper().parseText(json).items.find{it.metadata.name == microservice}
 	return item.status.dockerImageRepository
 }
@@ -24,8 +24,8 @@ node() {
 	
 	stage("deploy the test container") {
 		sh "oc get is -o json -n test > is.json"
-		def json = readFile('is.json')
-		def image = getImage(json)
+		def json = readFile ('is.json')
+		def image = getImage json:json, microservice:microserive
 		sh "oc create dc ${microservice} --image=${image}:PrepareForTesting -l app=${microservice} -n ${project}"
 		sh "oc set env dc/${microservice} -e JAEGER_SAMPLER_TYPE=const -e JAEGER_SAMPLER_PARAM=1 -e JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 -e JAEGER_AGENT_HOST=jaeger-agent.${project}.svc -n ${project}"
 		sh "oc set env dc/${microservice} -e PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice} -e PROJECT_API_DB_USER=postgres -e PROJECT_API_DB_PASSWORD=welcome1 -n ${project}" 
