4cd4d6d954f48fe146569f6e2c1536dd6eb724bc
Dennis Williams <dennis.williams@estafet.com>
2018-05-02 19:58:41 +0100

    define env vairables for deplyoment configuration.

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -21,8 +21,10 @@ node() {
 		def json = readFile('is.json');
 		def item = new groovy.json.JsonSlurper().parseText(json).items.find{it.metadata.name == microservice}
 		def image = item.status.dockerImageRepository
-		println image
-		//sh "oc create dc ${microservice} --image=${image}:PrepareForTesting"
+		sh "oc create dc ${microservice} --image=${image}:PrepareForTesting"
+		sh "oc set env dc/${microservice} -e JAEGER_SAMPLER_TYPE=const -e JAEGER_SAMPLER_PARAM=1 -e JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 -e JAEGER_AGENT_HOST=jaeger-agent.${project}.svc"
+		sh "oc set env dc/${microservice} -e PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice} -e PROJECT_API_DB_USER=postgres -e PROJECT_API_DB_PASSWORD=welcome1" 
+		sh "oc set env dc/${microservice} -e JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616 -e JBOSS_A_MQ_BROKER_USER=amq -e JBOSS_A_MQ_BROKER_PASSWORD=amq"
 		//openshiftDeploy namespace: project, deploymentConfig: microservice, waitTime: '300000'
 	}
   	  
