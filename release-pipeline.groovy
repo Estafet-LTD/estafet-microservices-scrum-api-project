ac92ccb7ae9baba6200c7721e64321c8390a1d29
Dennis Williams <dennis.williams@estafet.com>
2018-05-02 21:01:01 +0100

    break it out

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -20,9 +20,9 @@ node() {
 		sh "oc get is -o json -n ${project} > is.json"
 		def json = readFile('is.json');
 		def item = new groovy.json.JsonSlurper().parseText(json).items.find{it.metadata.name == microservice}
-		def image = item.status.dockerImageRepository
+		def image = "${item.status.dockerImageRepository}:PrepareForTesting"
         println image
-		sh "oc create dc ${microservice} --image=${image}:PrepareForTesting -n ${project}"
+		sh "oc create dc ${microservice} --image=${image} -n ${project}"
 		sh "oc set env dc/${microservice} -e JAEGER_SAMPLER_TYPE=const -e JAEGER_SAMPLER_PARAM=1 -e JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 -e JAEGER_AGENT_HOST=jaeger-agent.${project}.svc -n ${project}"
 		sh "oc set env dc/${microservice} -e PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice} -e PROJECT_API_DB_USER=postgres -e PROJECT_API_DB_PASSWORD=welcome1 -n ${project}" 
 		sh "oc set env dc/${microservice} -e JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616 -e JBOSS_A_MQ_BROKER_USER=amq -e JBOSS_A_MQ_BROKER_PASSWORD=amq -n ${project}"
