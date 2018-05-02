00fcbacaf1b1db65fc4a6321989d342ba24cd8ef
Dennis Williams <dennis.williams@estafet.com>
2018-05-02 21:41:02 +0100

    using extrnal moethod that is not serializable

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -1,3 +1,11 @@
+@NonCPS
+def getImage() {
+	sh "oc get is -o json -n ${project} > is.json"
+	def json = readFile('is.json');
+	def item = new groovy.json.JsonSlurper().parseText(json).items.find{it.metadata.name == microservice}
+	return item.status.dockerImageRepository
+}
+
 node() {
 
 	def project = "test"
@@ -17,15 +25,12 @@ node() {
 	}
 	
 	stage("deploy the test container") {
-		sh "oc get is -o json -n ${project} > is.json"
-		def json = readFile('is.json');
-		def item = new groovy.json.JsonSlurperClassic().parseText(json).items.find{it.metadata.name == microservice}
-		def image = item.status.dockerImageRepository
+		def image = getImage()
         println image
-		sh "oc create dc ${microservice} --image=${image}:PrepareForTesting -n ${project}"
-		sh "oc set env dc/${microservice} -e JAEGER_SAMPLER_TYPE=const -e JAEGER_SAMPLER_PARAM=1 -e JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 -e JAEGER_AGENT_HOST=jaeger-agent.${project}.svc -n ${project}"
-		sh "oc set env dc/${microservice} -e PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice} -e PROJECT_API_DB_USER=postgres -e PROJECT_API_DB_PASSWORD=welcome1 -n ${project}" 
-		sh "oc set env dc/${microservice} -e JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616 -e JBOSS_A_MQ_BROKER_USER=amq -e JBOSS_A_MQ_BROKER_PASSWORD=amq -n ${project}"
+		//sh "oc create dc ${microservice} --image=${image}:PrepareForTesting -n ${project}"
+		//sh "oc set env dc/${microservice} -e JAEGER_SAMPLER_TYPE=const -e JAEGER_SAMPLER_PARAM=1 -e JAEGER_SAMPLER_MANAGER_HOST_PORT=jaeger-agent.${project}.svc:5778 -e JAEGER_AGENT_HOST=jaeger-agent.${project}.svc -n ${project}"
+		//sh "oc set env dc/${microservice} -e PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice} -e PROJECT_API_DB_USER=postgres -e PROJECT_API_DB_PASSWORD=welcome1 -n ${project}" 
+		//sh "oc set env dc/${microservice} -e JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616 -e JBOSS_A_MQ_BROKER_USER=amq -e JBOSS_A_MQ_BROKER_PASSWORD=amq -n ${project}"
 		//openshiftDeploy namespace: project, deploymentConfig: microservice, waitTime: '300000'
 	}
   	  
