30ed04110268913c5a428e244495dbf538709646
Dennis Williams <dennis.williams@estafet.com>
2018-05-06 15:04:59 +0100

    optimised pipeline with more concise descriptions

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -7,12 +7,15 @@ node("maven") {
 		git branch: "master", url: "https://github.com/Estafet-LTD/estafet-microservices-scrum-api-project"
 	}
 
-	stage("build and execute unit tests") {
-		sh "mvn clean test"
-		junit "**/target/surefire-reports/*.xml"	
+	stage("unit tests") {
+		try {
+			sh "mvn clean test"
+		} finally {
+			junit "**/target/surefire-reports/*.xml"
+		}
 	}
 
-	stage("update the database schema") {
+	stage("update database") {
 		sh "oc get pods --selector app=postgresql -o json -n ${project} > pods.json"
 		def json = readFile('pods.json');
 		def pod = new groovy.json.JsonSlurper().parseText(json).items[0].metadata.name
@@ -23,13 +26,10 @@ node("maven") {
 
 	stage("build & deploy container") {
 		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "3000000"
-	}
-  	  
-	stage("verify container deployment") {
-		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"	
+		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
 	}
 
-	stage("execute the container tests") {
+	stage("container tests") {
 		withEnv(
 			[	"PROJECT_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
 				"PROJECT_API_DB_USER=postgres", 
