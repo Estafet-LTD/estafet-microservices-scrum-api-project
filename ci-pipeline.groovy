ba1d244d6ac3143a3ffbd99d477ef4e04d415547
Dennis Williams <dennis.williams@estafet.com>
2018-05-09 17:36:36 +0100

    point the microservice to the localhost a-mq by default and set it to the dev a-mq when ready to test.

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -26,6 +26,7 @@ node("maven") {
 
 	stage("build & deploy container") {
 		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "300000"
+		sh "oc set env dc/${microservice} JBOSS_A_MQ_BROKER_URL=tcp://broker-amq-tcp.${project}.svc:61616"
 		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
 	}
 
@@ -43,6 +44,7 @@ node("maven") {
 					sh "mvn verify -P integration-test"
 				}
 			} finally {
+				sh "oc set env dc/${microservice} JBOSS_A_MQ_BROKER_URL=tcp://localhost:61616"
 				junit "**/target/failsafe-reports/*.xml"
 			}
 	}
