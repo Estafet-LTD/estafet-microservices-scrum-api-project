279c686af323e2743e6fbd04582026cec41dad95
Dennis Williams <dennis.williams@estafet.com>
2018-05-09 17:51:50 +0100

    reset amq prior to running conotainer tests

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -23,6 +23,11 @@ node("maven") {
 		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"psql -d ${microservice} -U postgres -f /tmp/drop-${microservice}-db.ddl\""
 		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"psql -d ${microservice} -U postgres -f /tmp/create-${microservice}-db.ddl\""
 	}
+	
+	stage("reset a-mq to purge topics") {
+		openshiftDeploy namespace: project, depCfg: "broker-amq", showBuildLogs: "true",  waitTime: "3000000"
+		openshiftVerifyDeployment namespace: project, depCfg: "broker-amq", replicaCount:"1", verifyReplicaCount: "true", waitTime: "300000"
+	}
 
 	stage("build & deploy container") {
 		openshiftBuild namespace: project, buildConfig: microservice, showBuildLogs: "true",  waitTime: "300000"
