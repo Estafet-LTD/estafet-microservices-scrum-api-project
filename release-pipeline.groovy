7f38d57e60a3a5fcb3ee686794f8eaf6279c355c
Dennis Williams <dennis.williams@estafet.com>
2018-05-10 01:12:06 +0100

    read directly into a variable

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -28,8 +28,7 @@ node {
 	}
 	
 	stage("deploy container") {
-		sh "oc get is -o json -n ${project} > istream.json"
-		def is = readFile('istream.json')
+		def is = sh script : "oc get is -o json -n ${project}"
 		def image = getImage (is, microservice)
 		def template = readFile ('test-deployment-config.json').replaceAll(/\$\{image\}/, image).replaceAll(/\$\{microservice\}/, microservice)
 		sh "oc get dc -o json -n test > dc.json"
