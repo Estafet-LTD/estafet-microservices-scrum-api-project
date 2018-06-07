f7339338222352b76905cf1297e37f8decbc27cb
Dennis Williams <dennyjoewilliams@yahoo.com>
2018-06-07 23:43:03 +0100

    modified ddl upload

--- pipelines/release-pipeline.groovy
+++ pipelines/release-pipeline.groovy
@@ -39,7 +39,7 @@ node('maven') {
 		def json = readFile('pods.json')
 		def pod = new groovy.json.JsonSlurper().parseText(json).items[0].metadata.name	
 		def template = readFile ('ddl/createdb.sh.template').replaceAll(/\$\{microservice\}/, microservice)
-		writeFile file:"createdb.sh", text:template
+		writeFile file:"ddl/createdb.sh", text:template
 		sh "oc rsync --no-perms=true --include=\"*.ddl\" --exclude=\"*\" ddl/ ${pod}:/tmp -n ${project}"	
 		sh "oc rsync --no-perms=true --include=\"createdb.sh\" --exclude=\"*\" ddl/ ${pod}:/tmp -n ${project}"	
 		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"chmod +x /tmp/createdb.sh\""
