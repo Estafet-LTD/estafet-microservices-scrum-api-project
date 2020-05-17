ef20b2277fe60b8e2aa8f186884b548888b88f20
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-05-17 16:16:45 +0100

    configuration changes

--- src/main/java/com/estafet/microservices/api/project/Application.java
+++ src/main/java/com/estafet/microservices/api/project/Application.java
@@ -23,7 +23,7 @@ public class Application extends SpringBootServletInitializer {
 
 	@Bean
 	public io.opentracing.Tracer jaegerTracer() {
-		return new com.uber.jaeger.Configuration("project-api",
+		return new com.uber.jaeger.Configuration(System.getenv("JAEGER_SERVICE_NAME"),
 				com.uber.jaeger.Configuration.SamplerConfiguration.fromEnv(),
 				com.uber.jaeger.Configuration.ReporterConfiguration.fromEnv())
 				.getTracer();
